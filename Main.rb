8c477c651dd19b5655b9ee18027822dcaab5cd78
Adrien Baudet <baudetad@gmail.com>
2018-12-22 18:29:50 +0100

    changed note render to add accidentals

--- Main.rb
+++ Main.rb
@@ -7,16 +7,21 @@ class Note
     @note = note
   end
   def render
-    hauteur = @note / 7
-    note = @note % 7
+    hauteur = @note / 12
+    note = @note % 12
     case note
       when 0 then txt = 'c'
-      when 1 then txt = 'd'
-      when 2 then txt = 'e'
-      when 3 then txt = 'f'
-      when 4 then txt = 'g'
-      when 5 then txt = 'a'
-      when 6 then txt = 'b'
+      when 1 then txt = 'cis'
+      when 2 then txt = 'd'
+      when 3 then txt = 'dis'
+      when 4 then txt = 'e'
+      when 5 then txt = 'f'
+      when 6 then txt = 'fis'
+      when 7 then txt = 'g'
+      when 8 then txt = 'gis'
+      when 9 then txt = 'a'
+      when 10 then txt = 'ais'
+      when 11 then txt = 'b'
     end
     if hauteur >= 0
       txt + ("'" * hauteur)
