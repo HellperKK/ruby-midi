dfaff470c2f128b3fcf7d0b61272acee87ba929c
Adrien Baudet <baudetad@gmail.com>
2018-12-23 12:33:17 +0100

    updated generator to make it more generalist

--- Test.rb
+++ Test.rb
@@ -5,24 +5,25 @@ end
 
 class Generator
   def initialize
-    @voice = []
-    @bass = []
+    @voices = []
     @notes = Mode.new([0, 2, 4, 5, 7, 9, 11], 12)
   end
   def generate(size)
-    @bass = Array.new(size){rand(0..6)}
-    @voice = @bass.map do |elem|
+    @voices << Array.new(size){rand(0..6)}
+    @voices << @voices[-1].map do |elem|
+      elem + pick_random([2, 4, 7, 9, 11, 14])
+    end
+    @voices << @voices[-1].map do |elem|
       elem + pick_random([2, 4, 7, 9, 11, 14])
     end
   end
   def export
-    voice = Portee.new
-    @voice.each{|i| voice.add(@notes[i])}
-    bass = Portee.new
-    @bass.each{|i| bass.add(@notes[i])}
     score = Score.new
-    score.add_portee(voice)
-    score.add_portee(bass)
+    @voices.each do |notes|
+      portee = Portee.new
+      portee.add_line(notes.map{|e| @notes[e]})
+      score.add_portee(portee)
+    end
     File.open("TestGenerate.ly", "w"){|file| file.write(score.render)}
   end
 end
