acc2ba38dad818de4529db5247f17c7c619e5cf1
Adrien Baudet <baudetad@gmail.com>
2018-12-25 14:24:15 +0100

    removed useless commentary

--- src/Portee.rb
+++ src/Portee.rb
@@ -4,11 +4,9 @@ class Portee
   end
   def add(note)
     @notes << note
-    # @notes << Note.new(note)
   end
   def add_line(notes)
     @notes += notes
-    # @notes += notes.map{|e| Note.new(e)}
   end
   def render
     notes = @notes.map{|e| e.render}.join(" ")
